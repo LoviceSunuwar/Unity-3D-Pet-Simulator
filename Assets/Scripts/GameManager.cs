8f5a57eafbf497dc97ef81028ac03afb598eb7c6
Lovice Sunuwar <53461002+LoviceSunuwar@users.noreply.github.com>
2024-01-19 01:11:15 -0500

    Created a method to have the timer, showing theimter in the ui by textmeshproUI text, remaining is the logic to stop the game after the timer is over.
    The text display logic was taken from the hyper casual game template that we created previously.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public bool isGameStarted = false;
+
+    public TextMeshProUGUI textscore;
 
     public int level = 1;
 
     public GameObject[] rooms;
 
     public GameObject[] tutorial;
+
+    public GameObject ScreenEnd;
+
+    int timerFull = 30;
+
 
     private void Awake()
     {
@@ -33,7 +41,21 @@ public class GameManager : MonoBehaviour
         {
             isGameStarted = true;
             tutorial[0].SetActive(false);
+            InvokeRepeating("SetTimer", 1, 1);
+
         }
+    }
+
+    public void SetTimer()
+    {
+        timerFull--;
+        textscore.text = timerFull.ToString();
+    }
+
+    public void RestartGame()
+    {
+        Application.LoadLevel(Application.loadedLevelName); // This allows to load the level , in this case last one that was loaded.
+
     }
 
 }
