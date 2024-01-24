812265c30e190d8f8aae84198dbd2c47104178ea
Lovice Sunuwar <53461002+LoviceSunuwar@users.noreply.github.com>
2024-01-23 23:03:11 -0500

    Created a script and added shop item for the menuUI the script is for menu. where the player can buy levels or the timer lenght, timer speed. For now it is the level to buy

--- Assets/Scripts/MainUI.cs
+++ Assets/Scripts/MainUI.cs
@@ -5,9 +5,33 @@ using TMPro;
 public class MainUI : MonoBehaviour
 {
     public TextMeshProUGUI nbCoinsText;
+    public int playerNbCoins;
+    int binCoins;
+    int intialPrice = 20;
+    int coinsActualPrice;
+    public TextMeshProUGUI coinsPriceText;
+    public TextMeshProUGUI coinsLevelText;
 
     private void Awake()
     {
-        nbCoinsText.text = PlayerPrefs.GetInt("nbCoins", 0).ToString();
+        playerNbCoins = PlayerPrefs.GetInt("nbCoins", 0);
+        nbCoinsText.text = playerNbCoins.ToString();
+        binCoins = PlayerPrefs.GetInt("coinsLevel", 1);
+        coinsActualPrice = intialPrice * binCoins;
+        coinsPriceText.text = coinsActualPrice + "PO";
+    }
+
+    public void IncrementCoinLevel()
+    {
+        if (playerNbCoins >= coinsActualPrice)
+        {
+            playerNbCoins -= coinsActualPrice;
+            PlayerPrefs.SetInt("nbCoins", playerNbCoins);
+            PlayerPrefs.SetInt("coinsLevel", PlayerPrefs.GetInt("coinsLevel", 1) +1);
+            binCoins = PlayerPrefs.GetInt("coinsLevel", 1);
+            coinsLevelText.text = "LEVEL " + PlayerPrefs.GetInt("coinsLevel", 1);
+            coinsActualPrice = intialPrice * binCoins;
+            coinsPriceText.text = coinsActualPrice + "PO";
+        }
     }
 }
