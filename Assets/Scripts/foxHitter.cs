374c5e0bef23e3343408edff9ac0110b9c900c1c
Lovice Sunuwar <53461002+LoviceSunuwar@users.noreply.github.com>
2024-01-23 22:00:38 -0500

    Code Cleanup Making it more readable

--- Assets/Scripts/foxHitter.cs
+++ Assets/Scripts/foxHitter.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using Unity.Burst.CompilerServices;
 
 public class FoxHitter : MonoBehaviour
 {
@@ -20,16 +21,27 @@ public class FoxHitter : MonoBehaviour
             Rigidbody obj_rb = hit.gameObject.GetComponent<Rigidbody>(); // we recovering the component of what the object we touched. the items in the apartment.
             // so that we have the access to the rigidbody properties of the tagged name hitted or hittable or what we interac with.
             HittableObjects ho = hit.gameObject.GetComponent<HittableObjects>();
-
-            obj_rb.isKinematic = false;
-            obj_rb.AddExplosionForce(50, transform.position + Vector3.down, 10); // its used when a war or fps game when u throw bomb and the objects will propel from the position.
-            Instantiate(hitParticles[Random.Range(0, hitParticles.Length)], hit.gameObject.transform.position, Quaternion.identity);
-            iTween.PunchScale(foxScore, new Vector3(1.25f, 1.25f, 1.25f), 0.3f);
-            int newScore = int.Parse(foxScore.GetComponent<TextMeshProUGUI>().text) + ho.points;
-            foxScore.GetComponent<TextMeshProUGUI>().text = newScore.ToString();
-            foxScore3D.text = "+" + ho.points;
-            Invoke("ResetText3D", 0.5f);
+            HitEffect(obj_rb, hit);
+            
         }
+    }
+
+    public void HitEffect(Rigidbody obj_rb, ControllerColliderHit hit)
+    {
+        // Effect produced when colided
+        obj_rb.isKinematic = false;
+        obj_rb.AddExplosionForce(50, transform.position + Vector3.down, 10); // its used when a war or fps game when u throw bomb and the objects will propel from the position.
+        Instantiate(hitParticles[Random.Range(0, hitParticles.Length)], hit.gameObject.transform.position, Quaternion.identity);
+        iTween.PunchScale(foxScore, new Vector3(1.25f, 1.25f, 1.25f), 0.3f);
+
+    }
+
+    public void Score3DEffect(HittableObjects ho)
+    {
+        int newScore = int.Parse(foxScore.GetComponent<TextMeshProUGUI>().text) + ho.points;
+        foxScore.GetComponent<TextMeshProUGUI>().text = newScore.ToString();
+        foxScore3D.text = "+" + ho.points;
+        Invoke("ResetText3D", 0.5f);
     }
 
     public void ResetText3D()
